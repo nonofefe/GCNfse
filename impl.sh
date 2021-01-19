b4b773c5ec4b697befa4764cc3e2cfef28b4c832
nonofefe <yojunp8632@gmail.com>
2021-01-20 01:36:47 +0900

    impl.shの追加

---
+++ impl.sh
@@ -0,0 +1,9 @@
+python run_node_cls.py --rate 0.9
+
+# while [ $a -lt 5 ]
+# do
+#     a=`expr $a + 1`
+#     b=`expr 4 \* $a`
+#     python3 train.py --n_train $b --patience 10 --epoch 200 --alpha 0.6 --times 300 --dataset cora
+# done
+# echo "" >> log.txt
