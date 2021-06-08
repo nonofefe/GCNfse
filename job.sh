b86cb339067324221b68ffc2c420d1ee6463f0eb
nonofefe <yojunp8632@gmail.com>
2021-06-08 15:57:37 +0900

    job.shの追加

---
+++ job.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#$ -cwd
+#$ -l s_gpu=1
+#$ -l h_rt=1:00:00
+. /etc/profile.d/modules.sh
+module load python/3.6.5
+module load cuda/10.0.130
+pip install -r requirements.txt
+pip install dgl-cu100
+sh impl.sh
